8ac419f059ad880625ec231b67f2eff13bcfd16a
XianFengWu1996 <46692996+XianFengWu1996@users.noreply.github.com>
2023-01-17 22:05:42 -0500

    initialize project

---
+++ joining-data/app.js
@@ -0,0 +1,16 @@
+const data = [10, 20, 30, 40, 50];
+
+const el = d3
+  .select('ul')
+  .selectAll('li')
+  .data(data)
+  .join(
+    (enter) => {
+      return enter.append('li').style('color', 'purple');
+    },
+    (update) => update.style('color', 'green'),
+    (exit) => exit.remove()
+  )
+  .text((d) => d);
+
+console.log(el);
